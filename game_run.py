ceadecc6b1e3e34652e15300f0dd14032c04b38b
GabrielAlexanderFariasDeLimaTeixeira <gafdlt.snf19@uea.edu.br>
2019-12-01 10:50:52 -0400

    Primeira versão com mobilidade e background

---
+++ game_run.py
@@ -0,0 +1,51 @@
+import pygame
+import random
+
+IMG_DIR = "images/"
+pygame.init()
+
+global win
+x = 50
+y = 50
+width = 40
+height = 60
+vel = 5
+
+win = pygame.display.set_mode((576, 288))
+bg = pygame.image.load(IMG_DIR+"sand_place.png").convert_alpha()
+char = pygame.draw.rect(win, (54, 182, 112), (x, y, width, height))
+pygame.display.set_caption("Turtlelorian")
+clock = pygame.time.Clock()
+
+
+def screen_redraw():
+    win.blit(bg, (0, 0))
+    pygame.draw.rect(win, (54, 182, 112), (x, y, width, height))
+    pygame.display.update()
+
+
+intro = True
+
+while intro:
+    clock.tick(27)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            intro = False
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT] and x > vel:
+        x -= vel
+    if keys[pygame.K_RIGHT] and x < 576 - vel - width:
+        x += vel
+    if keys[pygame.K_UP] and y > vel:
+        y -= vel
+    if keys[pygame.K_DOWN] and y < 288 - vel - height:
+        y += vel
+
+    # pygame.draw.rect(win, (54, 182, 112), (x, y, width, height))
+    screen_redraw()
+
+
+pygame.quit()
